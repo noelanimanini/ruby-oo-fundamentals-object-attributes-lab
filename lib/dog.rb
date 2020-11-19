897a016dce575f9228ad939f1411974f51889943
smolnreadytobrawl <noelani.villanueva@gmail.com>
2020-11-18 20:09:38 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Dog
+    def name
+      @name    
+    end
+
+    def name=(name)
+        @name = name 
+    end 
+
+    def breed
+        @breed
+    end 
+
+    def breed=(breed)
+        @breed = breed
+    end 
+end
+
